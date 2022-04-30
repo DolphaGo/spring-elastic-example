cacb263512f38a41408538c11f190121e378ea80
DolphaGo <adamdoha@naver.com>
2022-04-30 15:48:26 +0900

    feat: Create project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring & ELK Example
+
+> Version
+
+- SpringBoot: 2.6.7
+- ELK : 8.1.3
+- Jdk 11
---
+++ src/main/java/com/example/elk/SpringElkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElkExampleApplication.class, args);
+    }
+
+}
