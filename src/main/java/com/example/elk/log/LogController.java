d8a50a9aa7bba9b0d9063192ccd404de1edcca97
DolphaGo <adamdoha@naver.com>
2022-05-01 02:30:56 +0900

    [Log] Spring Log to ELK (#1)
    * feat: spring Log to logStash
    
    * feat: TCP와 Console 둘다 로그 출력할 수 있도록 설정

---
+++ src/main/java/com/example/elk/log/LogController.java
@@ -0,0 +1,46 @@
+package com.example.elk.log;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/api/v1")
+@RestController
+public class LogController {
+
+    @GetMapping("/log")
+    public String get(@RequestParam(value = "message", required = false) String message) {
+        final String msg = StringUtils.hasText(message) ? message : "Please type message";
+        log.info("[DolphaGo] logstash log test = {}", msg);
+        return msg;
+    }
+
+    @PostMapping("/log")
+    public String post(@RequestParam(value = "message", required = false) String message) {
+        final String msg = StringUtils.hasText(message) ? message : "Please type message";
+        log.info("[DolphaGo] logstash log test = {}", msg);
+        return msg;
+    }
+
+    @PutMapping("/log")
+    public String put(@RequestParam(value = "message", required = false) String message) {
+        final String msg = StringUtils.hasText(message) ? message : "Please type message";
+        log.info("[DolphaGo] logstash log test = {}", msg);
+        return msg;
+    }
+
+    @DeleteMapping("/log")
+    public String delete(@RequestParam(value = "message", required = false) String message) {
+        final String msg = StringUtils.hasText(message) ? message : "Please type message";
+        log.info("[DolphaGo] logstash log test = {}", msg);
+        return msg;
+    }
+}
